391776b5de48447b81f8c6bc0d1a715d066476ff
Abhay kumar Sharma <abhayandblue@gmail.com>
2025-04-21 07:58:34 +0000

    Added PHP + HTML interface

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP with HTML</title>
+</head>
+<body>
+    <h1>Hello from PHP!</h1>
+    <p>The current server time is:</p>
+    <p><?php echo date("Y-m-d H:i:s"); ?></p>
+</body>
+</html>
